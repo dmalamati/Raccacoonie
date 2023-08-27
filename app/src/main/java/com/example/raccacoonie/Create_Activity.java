b962a3b90eada0da74d95f2c5b37a732e7d38e20
tsernik <steliostsern@yahoo.com>
2023-08-27 21:34:52 +0300

    ADD PHOTO WORKS WITH SOME CHANGES TO THE LAYOUT

--- app/src/main/java/com/example/raccacoonie/Create_Activity.java
+++ app/src/main/java/com/example/raccacoonie/Create_Activity.java
@@ -1,23 +1,50 @@
 package com.example.raccacoonie;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Create_Activity extends AppCompatActivity implements RecyclerViewInterface{
 
     int TEMP_CREATOR_ID = 2;
+    private EditText recipe_title;
+    private int id_post;
+    private Button share_recipe_button;
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +57,23 @@ public class Create_Activity extends AppCompatActivity implements RecyclerViewIn
         AutoCompleteTextView country = findViewById(R.id.autoCompleteTextView_country);
         ArrayAdapter<String> country_adapter= new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,countries);
         country.setAdapter(country_adapter);
-        Button share_recipe_button = findViewById(R.id.button_share);
+        share_recipe_button = findViewById(R.id.button_share);
         ImageButton search_button= findViewById(R.id.search_button);
         ImageButton home_button= findViewById(R.id.home_button);
-        EditText recipe_title = findViewById(R.id.editText_recipe_title);
         EditText ingredients = findViewById(R.id.editText_recipe_ingredients);
         EditText execution = findViewById(R.id.editText_recipe_execution);
         Spinner category_spinner = findViewById(R.id.spinner_category);
         Spinner tag_spinner = findViewById(R.id.spinner_tag);
+        recipe_title = findViewById(R.id.editText_recipe_title);
+        FrameLayout fragmentContainerBox = findViewById(R.id.bottom_container);
+
+
 
         DatabaseHandler myHandler = new DatabaseHandler(this,1);
         RecyclerAdapter adapter=new RecyclerAdapter(this,this);
         SharedPreferences prefs = getSharedPreferences("MyPrefs",0);
-        int creator_id = prefs.getInt("id",-1);
+         int creator_id = prefs.getInt("id",-1);
+
 
 
 
@@ -93,6 +124,7 @@ public class Create_Activity extends AppCompatActivity implements RecyclerViewIn
             }
         });
 
+
         share_recipe_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -103,69 +135,140 @@ public class Create_Activity extends AppCompatActivity implements RecyclerViewIn
 
                     //Toast.makeText(Create_Activity.this, String.valueOf(myHandler.recipe_count()), Toast.LENGTH_SHORT).show();
                    // myHandler.printRecipes_db();
+
+
                     Toast.makeText(Create_Activity.this, "Please add all the necessary fields before sharing", Toast.LENGTH_SHORT).show();
 
 
-                }else
-                {
+                }else {
 
                     String recipe_title_str = recipe_title.getText().toString();
-                   String execution_str = execution.getText().toString();
-                   String ingredients_str = ingredients.getText().toString();
-                   String country_str = country.getText().toString();
+                    String execution_str = execution.getText().toString();
+                    String ingredients_str = ingredients.getText().toString();
+                    String country_str = country.getText().toString();
 
-                   int dietaryStatus = 0;
+                    int dietaryStatus = 0;
 
 
-                   if (tag_spinner.getSelectedItem().toString().equals("Pescetarian"))
-                   {
-                       dietaryStatus = 1;
-                   }else if(tag_spinner.getSelectedItem().toString().equals("Vegetarian"))
-                   {
-                       dietaryStatus = 2;
-                   }else if (tag_spinner.getSelectedItem().toString().equals("Vegan"))
-                   {
-                       dietaryStatus = 3;
-                   }
+                    if (tag_spinner.getSelectedItem().toString().equals("Pescetarian")) {
+                        dietaryStatus = 1;
+                    } else if (tag_spinner.getSelectedItem().toString().equals("Vegetarian")) {
+                        dietaryStatus = 2;
+                    } else if (tag_spinner.getSelectedItem().toString().equals("Vegan")) {
+                        dietaryStatus = 3;
+                    }
 
-                   String category = "Dinner";
-                   if (category_spinner.getSelectedItem().toString().equals("Snack"))
-                    {
+                    String category = "Dinner";
+                    if (category_spinner.getSelectedItem().toString().equals("Snack")) {
                         category = "Snack";
-                    }else if (category_spinner.getSelectedItem().toString().equals("Dessert"))
-                   {
-                       category = "Dessert";
-                   }else if (category_spinner.getSelectedItem().toString().equals("Drink"))
-                   {
-                       category = "Drink";
-                   }
+                    } else if (category_spinner.getSelectedItem().toString().equals("Dessert")) {
+                        category = "Dessert";
+                    } else if (category_spinner.getSelectedItem().toString().equals("Drink")) {
+                        category = "Drink";
+                    }
 
                     Log.d("CREATOR ID TRANSMITTED TO THE ADAPTER", String.valueOf(creator_id));
-                   Recipe user_recipe = new Recipe(creator_id,recipe_title_str,"nullpic",execution_str,ingredients_str,category,dietaryStatus, country_str);
-                   //todo: add pictures, for now "nullpic" works fine
-                    Log.d("ID FROM RECIPE",String.valueOf(user_recipe.creator_id));
-                    Log.d("ID FROM RECIPE GETTER",String.valueOf(user_recipe.getCreator_id()));
+                    Recipe user_recipe = new Recipe(creator_id, recipe_title_str, "nullpic", execution_str, ingredients_str, category, dietaryStatus, country_str);
+                    //todo: add pictures, for now "nullpic" works fine
+                    Log.d("ID FROM RECIPE", String.valueOf(user_recipe.creator_id));
+                    Log.d("ID FROM RECIPE GETTER", String.valueOf(user_recipe.getCreator_id()));
 
 
                     //backend for recipe input
 
-                    int recipe_id = submitRecipe(user_recipe,myHandler);
-                    Log.d("ID FROM RECIPE",String.valueOf(user_recipe.creator_id));
-                    Log.d("ID FROM RECIPE GETTER",String.valueOf(user_recipe.getCreator_id()));
+                    int recipe_id = submitRecipe(user_recipe, myHandler);
+                    Log.d("ID FROM RECIPE", String.valueOf(user_recipe.creator_id));
+                    Log.d("ID FROM RECIPE GETTER", String.valueOf(user_recipe.getCreator_id()));
 
-                   adapter.ogrecipes.add(user_recipe);
-                   Log.d("CREATOR ID PASSED",String.valueOf(adapter.ogrecipes.get(adapter.ogrecipes.size()-1).creator_id));
-                   adapter.post_id.add(recipe_id);
-                   adapter.notifyItemInserted(adapter.getItemCount()-1);
+                    adapter.ogrecipes.add(user_recipe);
+                    Log.d("CREATOR ID PASSED", String.valueOf(adapter.ogrecipes.get(adapter.ogrecipes.size() - 1).creator_id));
+                    adapter.post_id.add(recipe_id);
+                    adapter.notifyItemInserted(adapter.getItemCount() - 1);
 
+                    Log.d("POSTS", String.valueOf(id_post));
+                    PhotoUploadFragment photoUploadFragment = new PhotoUploadFragment();
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.bottom_container, photoUploadFragment);
+                    transaction.addToBackStack(null);
+                    transaction.commit();
 
-
+                    findViewById(R.id.bottom_container).setVisibility(View.VISIBLE);
 
                 }
+
+
+
             }
+
+
+
+
+
+
+
+
+
         });
+        id_post=adapter.post_id.size()+1;
 
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ( resultCode == RESULT_OK && data != null) {
+            Uri imageUri = data.getData();
+
+
+            saveImageToAppDirectory(imageUri,id_post);
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+    private void saveImageToAppDirectory(Uri imageUri,int title) {
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(imageUri);
+            Bitmap imageBitmap = BitmapFactory.decodeStream(inputStream);
+
+            if (imageBitmap != null) {
+                saveBitmapToInternalStorage(this,imageBitmap, String.valueOf(title));
+                Toast.makeText(this, "Image saved successfully", Toast.LENGTH_SHORT).show();
+                Intent loadHomeActivity=new Intent(this,Home_Activity.class);
+                startActivity(loadHomeActivity);
+                findViewById(R.id.bottom_container).setVisibility(View.GONE);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Failed to save image", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void saveBitmapToInternalStorage(Context context, Bitmap bitmap, String fileName) {
+        try {
+            FileOutputStream fos = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            Toast.makeText(this,fileName,Toast.LENGTH_SHORT).show();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static Bitmap loadBitmapFromInternalStorage(Context context, String fileName) {
+        File file = new File(context.getFilesDir(), fileName);
+        if (file.exists()) {
+            return BitmapFactory.decodeFile(file.getAbsolutePath());
+        }
+        return null;
+    }
+
+
     public int submitRecipe(Recipe r, DatabaseHandler handler)
     {
         Toast.makeText(this, "Recipe was created", Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/com/example/raccacoonie/PhotoUploadFragment.java
@@ -0,0 +1,44 @@
+package com.example.raccacoonie;
+
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class PhotoUploadFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_photo_upload, container, false);
+        ImageButton add_photo_button = rootView.findViewById(R.id.imageButton_add_photo);
+
+        add_photo_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 3);
+
+
+
+            }
+        });
+
+
+
+
+
+
+
+        return rootView;
+    }
+}
